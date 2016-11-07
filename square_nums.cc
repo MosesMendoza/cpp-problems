aa75fdb89fa9c3180070b6d4ee8f75c9df17ea95
Moses Mendoza <mendoza.moses@gmail.com>
2016-11-07 15:32:57 -0800

    4.6 square nums
    a program that displays the first 20 square numbers
    
    Signed-off-by: Moses Mendoza <mendoza.moses@gmail.com>

---
+++ square_nums.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* A program to display the first 20 square numbers. A square number being the
+product of two equal integers */
+
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 20; i++){
+        cout << i * i << "\n";        
+    }
+}
+
